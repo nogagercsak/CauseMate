ca64bc3eb8358bad8ac3c876d86585774c902b7f
Noga Gercsak <nogah0317@gmail.com>
2024-03-16 15:02:28 -0400

    Initial Commit

---
+++ CauseMate/CauseMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CauseMateApp.swift
+//  CauseMate
+//
+//  Created by Noga Gercsak on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CauseMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
